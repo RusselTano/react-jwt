c5c039ff4083a8b82e7c90dcf44b76ada075fa40
Erwan Vallée <erwanp.vallee@gmail.com>
2024-02-13 15:32:55 +0100

    maj

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
     <footer
       className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}
     >
-      <p>Copyright © 2023 Dyma, Inc.</p>
+      <p>Copyright © 2025 Dyma, Inc.</p>
     </footer>
   );
 }
